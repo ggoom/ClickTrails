6e580a3617cd1d0d00c4596239b45e081c5e4025
Kathryn Jin <kjkjkathryn@gmail.com>
2023-04-07 16:21:57 -0400

    filter data in popup to current tab URL

--- popup.js
+++ popup.js
@@ -1,40 +1,59 @@
 const downloadButton = document.getElementById("downloadData");
-downloadButton.addEventListener("click", () => {
-    chrome.storage.local.get(null, function(items) { // null implies all items
-        // Convert object to a string.
-        var result = JSON.stringify(items, null, 2);
+
+chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+    const url = tabs[0].url;
+    const tabURL = new URL(url.replace(/\/$/, "")); //.split(/[?#]/)[0]
+    const tabHostname = tabURL.hostname;
+
+    downloadButton.addEventListener("click", () => {
+        chrome.storage.local.get(null, function (items) {
+            // null implies all items
+            const linksAsArray = Object.entries(items["click_data"]);
+            const filteredLinks = linksAsArray.filter(([key, value]) => key === tabHostname);
     
-        // Save as file
-        var url = 'data:application/json;base64,' + btoa(result);
-        chrome.downloads.download({
-            url: url,
-            filename: 'ext_data.json'
+            // Convert object to a string.
+            var result = JSON.stringify(filteredLinks, null, 2);
+    
+            // Save as file
+            var url = "data:application/json;base64," + btoa(result);
+            chrome.downloads.download({
+                url: url,
+                filename: "ext_data.json",
+            });
         });
     });
-});
+    
+    chrome.storage.local.get(["button_data"], function (items) {
+        const buttonsAsArray = Object.entries(items["button_data"]);
+        const filteredLinks = buttonsAsArray.filter(([key, value]) => key === tabHostname);
+    
+        // Convert object to a string.
+        var result = JSON.stringify(filteredLinks, null, 2);
+    
+        const container = document.getElementById("container-buttons");
+        container.innerHTML = `<pre>
+            <code>
+                ${result}
+            </code>
+        </pre>`;
+    });
+    
+    chrome.storage.local.get(["click_data"], function (items) {
+        const linksAsArray = Object.entries(items["click_data"]);
+        const filteredLinks = linksAsArray.filter(
+            ([key, value]) => key === tabHostname
+        );
+    
+        // Convert object to a string.
+        var result = JSON.stringify(filteredLinks, null, 2);
+    
+        const container = document.getElementById("container-links");
+        container.innerHTML = `<pre>
+            <code>
+                ${result}
+            </code>
+        </pre>`;
+    });
+})
 
-chrome.storage.local.get(['button_data'], function(items) {
-    // Convert object to a string.
-    var result = JSON.stringify(items, null, 2);
 
-    const container = document.getElementById("container-buttons");
-    container.innerHTML =
-    `<pre>
-        <code>
-            ${result}
-        </code>
-    </pre>`
-});
-
-chrome.storage.local.get(['click_data'], function(items) {
-    // Convert object to a string.
-    var result = JSON.stringify(items, null, 2);
-
-    const container = document.getElementById("container-links");
-    container.innerHTML =
-    `<pre>
-        <code>
-            ${result}
-        </code>
-    </pre>`
-});
